4f081996daa07421454af1bb9c75a8f3585bba64
Zanmatox <cheungkevin@hotmail.fr>
2021-01-31 00:05:29 +0100

    Fonction recherche des produits

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -7,6 +7,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Product;
+use App\Form\SearchType;
+use App\Classe\Search;
+use Symfony\Component\HttpFoundation\Request;
 
 
 class ProductController extends AbstractController
@@ -20,11 +23,21 @@ class ProductController extends AbstractController
     /**
      * @Route("/nos-produits", name="products")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $products = $this->entityManager->getRepository(Product::class)->findAll();
+        
+        $search = new search();
+        $form = $this->createForm(SearchType::class, $search);
+        
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $products = $this->entityManager->getRepository(Product::class)->findWithSearch($search);
+        }
+        
         return $this->render('product/index.html.twig',[
-            'products' => $products
+            'products' => $products,
+            'form' => $form->createView() 
         ]);
     }
 
--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\Product;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use App\Classe\Search;
 
 /**
  * @method Product|null find($id, $lockMode = null, $lockVersion = null)
@@ -18,6 +19,34 @@ class ProductRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Product::class);
     }
+
+
+    /**
+     * Requête qui me permet de recupérer les produits en fonction de la recherche de l'utilsateur
+     * @return Product[]
+     */
+    public function findWithSearch(Search $search)
+    {
+        $query=$this
+        ->createQueryBuilder('p')
+        ->select('c','p')
+        ->join('p.category','c');
+
+        if (!empty($search->categories)){
+            $query = $query
+            ->andWhere('c.id IN (:categories)')
+            ->setParameter('categories', $search->categories);
+        }
+
+        if (!empty($search->string)){
+            $query=$query
+            ->andWhere('p.name LIKE :string')
+            ->setParameter('string', "%{$search->string}%");
+        }
+
+        return $query->getQuery()->getResult();
+    }
+
 
     // /**
     //  * @return Product[] Returns an array of Product objects
