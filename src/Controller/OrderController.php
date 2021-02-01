8e62721bcb839a32e6f3474bb75342e09a75f27d
Zanmatox <cheungkevin@hotmail.fr>
2021-02-01 14:19:01 +0100

    Début stocker information

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -8,6 +8,8 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Form\OrderType;
 use App\Classe\Cart;
 use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Order;
+
 
 class OrderController extends AbstractController
 {
@@ -24,16 +26,58 @@ class OrderController extends AbstractController
             'user' => $this->getuser()
         ]);
 
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            dd($form->getData());
-        }
-
         return $this->render('order/index.html.twig', [
             'form' => $form ->createView(),
             'cart' => $cart->getFull()
         ]);
     }
-} 
+    
+    /**
+     * @Route("/commande/recapitulatif", name="order_recap")
+     */
+    public function add(Cart $cart, Request $request): Response
+    { 
+
+        $form = $this->createForm(OrderType::class, null, [
+            'user' => $this->getuser()
+        ]);
+
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $date = new \DateTime();
+            $carriers = $form->get('carriers')->getData();
+            $delivery = $form->get('addresses')->getData();
+            $delivery_content = $delivery->getFirstname().' '.$delivery->getLastname();
+            $delivery_content .= '<br/>'.$delivery->getPhone();
+            if ($delivery->getCompany())
+            {
+                $delivery_content .= '<br/>'.$delivery->getCompany();
+            }
+            $delivery_content .= '<br/>'.$delivery->getAddress();
+            $delivery_content .= '<br/>'.$delivery->getPostal().' '.$delivery->getCity();
+            $delivery_content .= '<br/>'.$delivery->getCountry();
+
+
+            $order = new Order();
+            $order->setUser($this->getUser());
+            $order->setCreatedAt($date);
+            $order->setCarrierName($carriers->getName());
+            $order->setCarrierPrice($carriers->getPrice());
+            $order->setDelivery($delivery_content);
+            $order->setIsPaid(0);
+
+            foreach ($cart->getFull() as $product) {
+                dd($product);
+            }
+
+
+
+        }
+
+        return $this->render('order/add.html.twig', [
+            'cart' => $cart->getFull()
+        ]);
+    }
+}  
